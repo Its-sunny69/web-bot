7d5b9e5d0cb63f57262a91fbaf2002aa9f2b3b4d
OmkarAnbhule <omkaranbhule75@gmail.com>
2025-07-17 21:28:48 +0530

    update minor changes

--- src/telegram_bot/services/bot.py
+++ src/telegram_bot/services/bot.py
@@ -1,10 +1,19 @@
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler
 from .dispatcher import register_commands
+from telegram_bot.commands.start import start_command
+from telegram_bot.commands.login import login_command
 from django.conf import settings
+
+
 def build_bot():
-    from decouple import config
     token = settings.TELEGRAM_BOT_TOKEN
     app = Application.builder().token(token).build()
     register_commands(app)
-    
     return app
+
+
+def run_bot():
+    application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("login", login_command))
+    application.run_polling()
