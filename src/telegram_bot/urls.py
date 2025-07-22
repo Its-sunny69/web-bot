efed68d95fd1f39d8cb432538d18f762e6fe04bc
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-07-22 18:29:40 +0530

    webhook changes

--- src/telegram_bot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .services.webhook import handle_telegram_webhook
-
-
-urlpatterns = [
-    path('telegram-webhook/', handle_telegram_webhook, name='handle_telegram_webhook'),
-]
