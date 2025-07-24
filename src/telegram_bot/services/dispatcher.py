5662d33a8de4bd573f15bb006d65acf3736f7597
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-07-24 20:55:18 +0530

    webhook error fix

--- src/telegram_bot/services/dispatcher.py
+++ src/telegram_bot/services/dispatcher.py
@@ -2,9 +2,8 @@ from telegram.ext import CommandHandler
 from ..commands.start import start_command
 from ..commands.login import login_command
 from ..commands.logout import logout_command
-from ..commands.list_repos import list_repos_command
+
 def register_commands(app):
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("login", login_command))
     app.add_handler(CommandHandler("logout", logout_command))
-    app.add_handler(CommandHandler("listrepos", list_repos_command))
