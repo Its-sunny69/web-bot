1fc8ecbaaa0f1fb04bbc5d6724a85f6ae86d68de
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-07-24 21:43:08 +0530

    env and url set

--- src/telegram_bot/commands/login.py
+++ src/telegram_bot/commands/login.py
@@ -1,13 +1,21 @@
 from urllib.parse import urlencode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from django.conf import settings
+
 
 async def login_command(update, context):
     telegram_user_id = update.effective_user.id
-    print(telegram_user_id)
+    live_url = settings.SERVER_URL
     params = urlencode({"tg_id": telegram_user_id})
-    login_url = f"http://localhost:8000/api/auth/github/login?{params}"
+    login_url = f"{live_url}/api/auth/github/login?{params}"
    
+   # Create a button
+    keyboard = [
+        [InlineKeyboardButton("🔗 Connect GitHub", url=login_url)]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     await update.message.reply_text(
-         
-         f"Copy and open this link in your browser:\n{login_url}"
+        "Click the button below to connect your GitHub account:",
+        reply_markup=reply_markup
     )
